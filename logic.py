66171c00cfb5487cec833f0eb6b2107317bf24c6
Victor Aluko <victor.aluko@gmail.com>
2016-12-03 20:41:04 +0200

    Added Iyke's changes to the main branch

--- logic.py
+++ logic.py
@@ -2,8 +2,8 @@ mainboard = ''
 
 
 class Logic:
-    leftT = 350
-    rightT = 290
+    leftT = 330
+    rightT = 310
     goalMin = 110
     goalMax = 200
     goal_find_cnt = 0
@@ -15,7 +15,7 @@ class Logic:
         return balldetails != [0, 0, 0]
 
     def movetoball(self, balldetails):
-        if balldetails == [0,0,0]:
+        if balldetails == [0, 0, 0]:
             self.mainboard.circlearound(speed=55)
         else:
             self.mainboard.forwardspeed()
@@ -23,24 +23,41 @@ class Logic:
                 self.mainboard.turnleft(50)
             elif balldetails[0] < self.rightT:
                 self.mainboard.turnright(50)
-            elif balldetails[0] >= self.rightT and balldetails[0] <= self.leftT:
+            else:
                 self.mainboard.backwheel(0)
                 self.mainboard.forwardspeed()
 
+    def turnandshoot(self, goaldetails):
+        print ('goal:', goaldetails)
+        goal_scored = False
+
+        if goaldetails == [0, 0]:
+            self.mainboard.circlearound()
+        else:
+            self.mainboard.forwardspeed()
+            if goaldetails[0] > 325:
+                self.mainboard.turnleft()
+            elif goaldetails[0] < 315:
+                self.mainboard.turnright()
+            else:
+                self.mainboard.kick()
+                goal_scored = True
+        return goal_scored
+
     def aimandshoot(self, goaldetails):
-        print ('goal:',goaldetails)
+        print ('goal:', goaldetails)
         goal_scored = False
         if goaldetails == [0, 0]:
             self.goal_find_cnt = 0
             self.mainboard.circlearound(35)
-            #self.mainboard.turnleft(30)
+            # self.mainboard.turnleft(30)
         else:
-            #self.mainboard.forwardspeed()
-            #if goaldetails[0] > self.goalMax:
+            # self.mainboard.forwardspeed()
+            # if goaldetails[0] > self.goalMax:
             #    self.mainboard.turnleft(40)
-            #elif goaldetails[0] < self.goalMin:
+            # elif goaldetails[0] < self.goalMin:
             #    self.mainboard.turnright(40)
-            #elif (goaldetails[1] >= self.goalMin):
+            # elif (goaldetails[1] >= self.goalMin):
             if goaldetails[0] < self.goalMin:
                 self.mainboard.forwardspeed(30)
             elif (goaldetails[1] >= self.goalMin and goaldetails[1] <= self.goalMax):
